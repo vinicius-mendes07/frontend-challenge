bc21a8c2a3f54dc70b3352542c8d882e9d942f88
vinicius <vinicius.henrique.92@hotmail.com>
2023-05-24 10:41:48 -0300

    feat: creating header

---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,30 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import styled from "styled-components";
+import { CartIcon } from "./cart-icon";
+
+const CartCount = styled.span`
+    widtd: 17px;
+    height: 17px;
+    border-radius: 100%;
+    padding: 0 5px;
+    font-size: 10px;
+
+    background-color: var(--delete-color);
+    color: #fff;
+
+    margin-left: -10px;
+`
+
+const Container = styled.div`
+    position: relative;
+`
+
+export function CartControl() {
+    const {value} = useLocalStorage('cart-items')
+    return (
+        <Container>
+            <CartIcon />
+            {value.length && <CartCount>{value.length}</CartCount>}
+        </Container>
+    )
+}
