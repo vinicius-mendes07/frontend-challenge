046b6f383b4829d8c3a76fb100a2832b7e71c60f
vinicius <vinicius.henrique.92@hotmail.com>
2023-06-30 09:44:24 -0300

    feat: add navigation to cart page

--- capputeeno/src/components/cart-control.tsx
+++ capputeeno/src/components/cart-control.tsx
@@ -1,4 +1,6 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useRouter } from "next/navigation";
+import Router from "next/router";
 import styled from "styled-components";
 import { CartIcon } from "./icons/cart-icon";
 
@@ -15,14 +17,21 @@ const CartCount = styled.span`
     margin-left: -10px;
 `
 
-const Container = styled.div`
+const Container = styled.button`
     position: relative;
+    border: none;
+    background: transparent;
+    cursor: pointer;
 `
 
 export function CartControl() {
     const {value} = useLocalStorage('cart-items', [])
+    const router = useRouter()
+    const handleNavigateToCart = ()=> {
+        router.push('/cart')
+    }
     return (
-        <Container>
+        <Container onClick={handleNavigateToCart}>
             <CartIcon />
             {value.length > 0 && <CartCount>{value.length}</CartCount>}
         </Container>
