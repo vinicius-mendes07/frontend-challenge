96684b28b46c7fcdcdc5595b6082363558770d60
vinicius <vinicius.henrique.92@hotmail.com>
2023-06-30 09:43:48 -0300

    feat: add navigation to home

--- capputeeno/src/components/header.tsx
+++ capputeeno/src/components/header.tsx
@@ -34,6 +34,7 @@ const Logo = styled.a`
     font-weight: 400;
     font-size: 20px;
     line-height: 150%;
+    text-decoration: none;
 
     @media (min-width: ${props => props.theme.tabletBreakpoint}) {
         font-size: 24px;
@@ -48,7 +49,7 @@ export function Header(){
     const {setSearch, search} = useFilter()
     return (
         <TagHeader>
-            <Logo className={sairaStencil.className}>Capputeeno</Logo>
+            <Logo className={sairaStencil.className} href="/">Capputeeno</Logo>
             <div>
                 <PrimaryInputWSearchIcon
                     value={search}
